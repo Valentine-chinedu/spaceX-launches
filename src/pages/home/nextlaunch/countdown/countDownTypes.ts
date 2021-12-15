3a63d2bef0cca72337072d4f49da78461499af03
okosival <valentine11.dev@gmail.com>
2021-12-15 17:17:01 +0000

    removed app.css

--- src/pages/home/nextlaunch/countdown/countDownTypes.ts
+++
@@ -1,6 +0,0 @@
-export type CountdownProps = {
-	days: number;
-	hours: number;
-	minutes: number;
-	seconds: number;
-};
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './home/Home';
+export { default as Launches } from './launches/Launches';
+export { default as Rockets } from './rockets/Rockets';
+export { default as StarLink } from './starlink/StarLink';
