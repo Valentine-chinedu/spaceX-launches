a4ca17d5c2e84b6fa5a29e9339c2f40a32ba44e2
okosival <valentine11.dev@gmail.com>
2021-12-19 00:29:16 +0000

    fix: reimported launchhitroy chart

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { randomQuote } from '../components/other/muskQuotes';
 import { LaunchHistoryChart } from '../components/home/chart/LaunchHistoryChart';
 import Launches from '../components/home/launches/Launches';
-// import LaunchHistoryChart from './launchhistory/LaunchHistoryChart';
 import NextLaunch from '../components/home/nextlaunch/NextLaunch';
 import Rockets from '../components/home/rockets/Rockets';
 import Starlink from '../components/home/Starlink';
