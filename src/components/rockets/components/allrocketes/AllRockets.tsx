238fce2fcc5be0082c8f93c5b755ae4bcb015ecc
okosival <valentine11.dev@gmail.com>
2021-12-19 00:31:24 +0000

    created dynamic routes for each rocket

--- src/components/rockets/components/allrocketes/AllRockets.tsx
+++ src/components/rockets/components/allrocketes/AllRockets.tsx
@@ -1,43 +1,89 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import f1 from '../../../../../public/images/f1.png';
 import f9 from '../../../../../public/images/f9.png';
-import fh from '../../../../../public/images/fh.png';
+import fh from '../../../../../public/images/falconHeavy.png';
 import st from '../../../../../public/images/st.png';
+import { useRockets } from '../../../../services/Queries';
 
 interface Props {
 	rocketName: string;
 }
 
 const AllRockets = ({ rocketName }: Props) => {
-	// const rockets = [
-	// 	{ name: 'Falcon 1', media: falcon1, alt: 'rocket' },
-	// 	{ name: 'Falcon 9', media: falcon9, alt: 'rocket' },
-	// 	{ name: 'Falcon Heavy', media: falconHeavy, alt: 'rocket' },
-	// 	{ name: 'StarShip', media: Starship, alt: 'rocket' },
-	// ];
+	const { data } = useRockets();
+	console.log(data);
 	return (
 		<div
-			className={`flex justify-center items-start bg-green-800 ${
+			className={`flex justify-center items-end w-full space-x-20 ${
 				rocketName !== 'rockets' ? 'hidden' : ''
 			} `}
 		>
-			<div className={`flex flex-col items-center justify-end bg-red-300 `}>
-				<Image src={f1} alt='rocket' />
-				<h3>Falcon 1</h3>
-			</div>
-			<div className={`flex flex-col items-center justify-end bg-yellow-200 `}>
-				<Image className='' src={f9} alt='rocket' />
-				<h3>Falcon 9</h3>
-			</div>
-			<div className={`flex flex-col items-center justify-start bg-green-400 `}>
-				<Image className='bg-blue-800 h-[20]' src={fh} alt='rocket' />
-				<h3>Falcon heavy</h3>
-			</div>
-			<div className={` flex flex-col items-center justify-end bg-blue-600 `}>
-				<Image className='' src={st} alt='rocket' />
-				<h3>Starship</h3>
-			</div>
+			<Link
+				href={`/rockets/${data && data[0].id}`}
+				as={`/rockets/${data && data[0].name}`}
+			>
+				<a className={`w-full`}>
+					<Image
+						className='object-contain'
+						src={f1}
+						alt='rocket'
+						height={700}
+						width={500}
+						layout='responsive'
+					/>
+					<h3>Falcon 1</h3>
+				</a>
+			</Link>
+			<Link
+				href={`/rockets/${data && data[1].id}`}
+				as={`/rockets/${data && data[1].name}`}
+			>
+				<a className={`w-full `}>
+					<Image
+						className='object-contain'
+						src={f9}
+						alt='rocket'
+						height={900}
+						width={500}
+						layout='responsive'
+					/>
+					<h3>Falcon 9</h3>
+				</a>
+			</Link>
+			<Link
+				href={`/rockets/${data && data[2].id}`}
+				as={`/rockets/${data && data[2].name}`}
+			>
+				<a className={`w-full `}>
+					<Image
+						className='object-contain'
+						src={fh}
+						alt='rocket'
+						height={230}
+						width={100}
+						layout='responsive'
+					/>
+					<h3>Falcon Heavy</h3>
+				</a>
+			</Link>
+			<Link
+				href={`/rockets/${data && data[3].id}`}
+				as={`/rockets/${data && data[3].name}`}
+			>
+				<a className={`w-full`}>
+					<Image
+						className='object-contain'
+						src={st}
+						alt='rocket'
+						height={1500}
+						width={500}
+						layout='responsive'
+					/>
+					<h3>Starship</h3>
+				</a>
+			</Link>
 		</div>
 	);
 };
