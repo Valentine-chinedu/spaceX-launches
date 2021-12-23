34f04abaa0bcf3fd6bd2629f6f58757287558501
okosival <valentine11.dev@gmail.com>
2021-12-23 13:28:03 +0000

    added paddin and text size and color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link';
 
 const NavBar = () => {
 	return (
-		<div>
-			<Link href='/'>SpacexInfo</Link>
-			<div>
-				<Link href='/rockets/Rockets'>Rockets</Link>
-				<Link href='/Launches'>Launches</Link>
+		<div className='flex justify-between px-20 py-6 bg-zinc-700 text-xl text-gray-200'>
+			<Link href='/'>
+				<a className='uppercase text-2xl text-lime-500 font-bold'>SpacexInfo</a>
+			</Link>
+			<div className='w-auto space-x-20 '>
+				<Link href='/'>Home</Link>
+				<Link href='/Rockets'>Rockets</Link>
+				<Link href='Launches'>Launches</Link>
 				<Link href='/StarLink'>Starlink</Link>
 			</div>
 		</div>
