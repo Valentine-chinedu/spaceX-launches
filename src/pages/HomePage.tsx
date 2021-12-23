ee9a43663ed0d233e8a51eedbcded5b7488b42b8
okosival <valentine11.dev@gmail.com>
2021-12-23 12:47:21 +0000

    added animation to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,20 +4,27 @@ import { LaunchHistoryChart } from '../components/home/chart/LaunchHistoryChart'
 import Launches from '../components/home/launches/Launches';
 import NextLaunch from '../components/home/nextlaunch/NextLaunch';
 import Rockets from '../components/home/rockets/Rockets';
-import Starlink from '../components/home/Starlink';
+import { motion } from 'framer-motion';
+import { pageVariantsAnim } from '../animation';
+// import Starlink from '../components/home/Starlink';
 
 const HomePage = () => {
 	return (
-		<div>
+		<motion.div
+			initial='initial'
+			animate='in'
+			exit='out'
+			variants={pageVariantsAnim}
+		>
 			<NextLaunch />
 			<div>
 				<q>{randomQuote()} </q> - <span>Elon Musk</span>
 			</div>
 			<Launches />
 			<Rockets />
-			<Starlink />
+			{/* <Starlink /> */}
 			<LaunchHistoryChart />
-		</div>
+		</motion.div>
 	);
 };
 
