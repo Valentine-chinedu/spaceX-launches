b0875a9c7cde3d2259d66c6b1bbe51c9a8c7726d
okosival <valentine11.dev@gmail.com>
2021-12-24 07:07:08 +0000

    changed text color

--- src/components/home/nextlaunch/components/countdown/CountDown.tsx
+++ src/components/home/nextlaunch/components/countdown/CountDown.tsx
@@ -3,25 +3,25 @@ import { CountdownProps } from './countDownTypes';
 
 const CountDown = ({ days, hours, minutes, seconds }: CountdownProps) => {
 	return (
-		<div>
-			<div>
+		<div className='flex w-full h-full items-center justify-center space-x-20 text-7xl font-bold text-green-500 mb-10'>
+			<div className='flex flex-col items-center'>
 				<h2>{('0' + days).slice(-2)}</h2>
-				<h2>Days</h2>
+				<h2 className='text-lg text-gray-200'>Days</h2>
 			</div>
-			<h2>:</h2>
-			<div>
+			<h2 className='relative bottom-5'>:</h2>
+			<div className='flex flex-col items-center'>
 				<h2>{('0' + hours).slice(-2)}</h2>
-				<h2>Hours</h2>
+				<h2 className='text-lg text-gray-200'>Hours</h2>
 			</div>
-			<h2>:</h2>
-			<div>
+			<h2 className='relative bottom-5'>:</h2>
+			<div className='flex flex-col items-center'>
 				<h2>{('0' + minutes).slice(-2)}</h2>
-				<h2>Minutes</h2>
+				<h2 className='text-lg text-gray-200'>Minutes</h2>
 			</div>
-			<h2>:</h2>
-			<div>
+			<h2 className='relative bottom-5'>:</h2>
+			<div className='flex flex-col items-center'>
 				<h2>{('0' + seconds).slice(-2)}</h2>
-				<h2>Seconds</h2>
+				<h2 className='text-lg text-gray-200'>Seconds</h2>
 			</div>
 		</div>
 	);
