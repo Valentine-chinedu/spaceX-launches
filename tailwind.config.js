643b3aad82ac7fcec3813f8d5eaddc5763ad0f89
okosival <valentine11.dev@gmail.com>
2021-12-25 11:52:04 +0000

    added background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,tsx,jsx}', './index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				landing_img: "url('/images/landing_bg.jpg')",
+				launch_img: "url('/images/launch_bg2.jpg')",
+				rocket_img: "url('/images/rocket_bg.jpg')",
+				starlink_img: "url('/images/starlink_bg.jpg')",
+				starship_img: "url('/images/starship_bg.jpg')",
+			},
+		},
 	},
 	plugins: [],
 };
