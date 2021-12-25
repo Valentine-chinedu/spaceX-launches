f63a164278c4b5531ce508f5c7e31072f1765e4d
okosival <valentine11.dev@gmail.com>
2021-12-25 11:53:09 +0000

    removed bg color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,15 +2,25 @@ import Link from 'next/link';
 
 const NavBar = () => {
 	return (
-		<div className='flex justify-between px-20 py-6 bg-zinc-700 text-xl text-gray-200'>
+		<div className='flex absolute w-full justify-between px-20 py-6 text-xl'>
 			<Link href='/'>
-				<a className='uppercase text-2xl text-lime-500 font-bold'>SpacexInfo</a>
+				<a className='uppercase text-2xl text-stone-100 font-bold'>
+					SpacexInfo
+				</a>
 			</Link>
-			<div className='w-auto space-x-20 '>
-				<Link href='/'>Home</Link>
-				<Link href='/Rockets'>Rockets</Link>
-				<Link href='Launches'>Launches</Link>
-				<Link href='/StarLink'>Starlink</Link>
+			<div className='w-auto space-x-20 text-stone-50 h-full'>
+				<Link href='/'>
+					<a className='hover:border-b border-stone-50'>Home</a>
+				</Link>
+				<Link href='/Rockets'>
+					<a className='hover:border-b border-stone-50'>Rockets</a>
+				</Link>
+				<Link href='Launches'>
+					<a className='hover:border-b border-stone-50'>Launches</a>
+				</Link>
+				<Link href='/StarLink'>
+					<a className='hover:border-b border-stone-50'>Starlink</a>
+				</Link>
 			</div>
 		</div>
 	);
