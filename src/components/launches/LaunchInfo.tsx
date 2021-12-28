2502265e888384c9fffe46c6b5c34e695d7b1964
okosival <valentine11.dev@gmail.com>
2021-12-28 10:50:35 +0000

    completed styling

--- src/components/launches/LaunchInfo.tsx
+++ src/components/launches/LaunchInfo.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { RocketType } from '../../types';
 import noImage from '../../../public/images/noImage.png';
 import Link from 'next/link';
+import moment from 'moment';
 
 interface IProps {
 	name: string;
@@ -27,44 +28,59 @@ const LaunchInfo = ({
 	return (
 		<Link href={`/launch/${id}`}>
 			<a>
-				<div key={id}>
-					<div>
+				<div
+					key={id}
+					className='flex flex-col justify-between text-stone-100 bg-gray-800 w-full rounded mb-8 p-4'
+				>
+					<div className='flex justify-between items-center'>
 						<div>
-							<h1>{name}</h1>
-							<h3>{date}</h3>
+							<h1 className='text-2xl text-amber-900'>{name}</h1>
+							<h3 className='text-xs'>{moment(date).format('LLL')}</h3>
 						</div>
-						<h2>{number}</h2>
-					</div>
-
-					<div>
-						<span>ROCKET :</span>
-						<span>
-							{rocket === RocketType.f1
-								? 'Falcon 1'
-								: rocket === RocketType.f9
-								? 'Falcon 9'
-								: rocket === RocketType.fh
-								? 'Falcon Heavy'
-								: rocket === RocketType.starship
-								? 'Starship'
-								: ''}
-						</span>
-						<span className='object-contain h-20'>
+						<span className='object-contain '>
 							<Image
 								src={patch_small === null ? noImage : patch_small}
 								alt='image not found'
-								width={10}
-								height={10}
-								layout='responsive'
+								width={100}
+								height={100}
 							/>
 						</span>
 					</div>
-					<div>
-						<p>{details}</p>
+
+					<div className='flex mb-2'>
+						<div className='uppercase mr-2 text-gray-300'>
+							<h3>rocket :</h3>
+							<h3>status :</h3>
+						</div>
+
+						<div>
+							<h3>
+								{rocket === RocketType.f1
+									? 'Falcon 1'
+									: rocket === RocketType.f9
+									? 'Falcon 9'
+									: rocket === RocketType.fh
+									? 'Falcon Heavy'
+									: rocket === RocketType.starship
+									? 'Starship'
+									: ''}
+							</h3>
+							<h3
+								className={`capitalize ${
+									status === 'failed'
+										? 'text-red-700'
+										: status === 'successfull'
+										? 'text-green-600'
+										: ''
+								}`}
+							>
+								{status}
+							</h3>
+						</div>
 					</div>
-					<div>
-						<span>status :</span>
-						<span>{status}</span>
+					<div className='flex justify-between items-end'>
+						<p className='text-sm'>{details}</p>
+						<h2>#{number}</h2>
 					</div>
 				</div>
 			</a>
