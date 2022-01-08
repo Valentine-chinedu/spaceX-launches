b03fda82d56fd9cc0202d3b46180697d04f2a006
okosival <valentine11.dev@gmail.com>
2022-01-08 13:05:52 +0000

    completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
+
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import NavBar from '../components/NavBar';
@@ -11,14 +11,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
 	return (
 		<>
-			<Head>
-				<title>SpaceX Info</title>
-
-				<meta property='og:image' content='/spacex-img.jpg' />
-				<meta property='og:image:type' content='image/jpeg' />
-				<meta property='og:image:width' content='200' />
-				<meta property='og:image:height' content='200' />
-			</Head>
 			<NavBar />
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import Head from 'next/head';
 import React from 'react';
 import HomePage from './HomePage';
 
 const Home = () => {
-	return <HomePage />;
+	return (
+		<>
+			<Head>
+				<title>SpaceX Project</title>
+
+				<title>SpaceX Info</title>
+
+				<meta property='og:image' content='/spacex-img.jpg' />
+				<meta property='og:image:type' content='image/jpeg' />
+				<meta property='og:image:width' content='200' />
+				<meta property='og:image:height' content='200' />
+			</Head>
+			<HomePage />
+		</>
+	);
 };
 
 export default Home;
