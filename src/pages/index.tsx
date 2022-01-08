6270f63b441e35e717c747c5fa22ac5b25213cfb
okosival <valentine11.dev@gmail.com>
2022-01-08 12:46:07 +0000

    fix: fixed error with meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import HomePage from './HomePage';
 const Home = () => {
 	<Head>
 		<title>SpaceX Project</title>
+		<meta property='og:image' content='/spacex-img.jpg' />
+		<meta property='og:image:type' content='image/jpeg' />
+		<meta property='og:image:width' content='200' />
+		<meta property='og:image:height' content='200' />
 	</Head>;
 	return <HomePage />;
 };
