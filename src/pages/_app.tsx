9774610720659ce8d3605bef5e5d9b909818f114
okosival <valentine11.dev@gmail.com>
2022-01-08 14:24:11 +0000

    final commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,24 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import NavBar from '../components/NavBar';
 
 import 'tailwindcss/tailwind.css';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	const queryClient = new QueryClient();
 
 	return (
 		<>
+			<Head>
+				<title>SpaceX Info</title>
+
+				<meta
+					property='og:image'
+					content='http://space-x-launches-iota.vercel.app/spacex-img.jpg'
+				/>
+				<meta property='og:image:type' content='image/jpeg' />
+				<meta property='og:image:width' content='200' />
+				<meta property='og:image:height' content='200' />
+			</Head>
 			<NavBar />
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
-import Head from 'next/head';
 import React from 'react';
 import HomePage from './HomePage';
 
 const Home = () => {
 	return (
 		<>
-			<Head>
-				<title>SpaceX Info</title>
-
-				<meta
-					property='og:image'
-					content='http://space-x-launches-iota.vercel.app/spacex-img.jpg'
-				/>
-				<meta property='og:image:type' content='image/jpeg' />
-				<meta property='og:image:width' content='200' />
-				<meta property='og:image:height' content='200' />
-			</Head>
 			<HomePage />
 		</>
 	);
